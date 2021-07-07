71fecb6b1ba88da28d075c1d2078ab859777a141
Mr Markutan <mrmarkutan@gmail.com>
2021-07-07 14:36:00 +0300

    Added RecipeService and RecipeServiceImpl and RecipeBootstrap.

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    private String getIndexPage(){
-        System.out.println("Some message to say... 55555555");
+    private String getIndexPage(Model model){
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
